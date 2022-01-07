5ac88b648265e34880d6db27706c0e85f5cef8c6
Nikodem <nikodemmarek11@gmail.com>
2022-01-07 10:29:45 +0100

    feat: game loop

---
+++ src/game/game.ts
@@ -0,0 +1,29 @@
+import { Container } from 'pixi.js'
+
+export default class Game {
+    container: Container
+    state: number = Game.states.INIT
+    private update: (delta: number) => void
+
+    public static states = {
+        INIT: 0,
+        RUNNING: 1,
+        PAUSED: 1,
+        FINISHED: 2
+    }
+
+    constructor(
+        container: Container,
+        update: (delta: number) => void
+    ) {
+        this.container = container
+        this.update = update
+    }
+
+    start(FPS: number = 30) {
+        const frameTime = 1000 / FPS
+
+        this.state = Game.states.RUNNING
+        setInterval(() => { if(this.state == Game.states.RUNNING) this.update(frameTime) }, frameTime)
+    }
+}
