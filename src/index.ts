86bc96cafe7231c5da9db8497f6d9cf9fbfdd38b
Nikodem <nikodemmarek11@gmail.com>
2022-02-02 18:30:29 +0100

    feat: removed pointlessly created game-menus.ts and added a dummy game

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import { Application } from 'pixi.js'
+import { Application, Graphics } from 'pixi.js'
 import { ImagesProvider } from './assets-provider'
-import { GAME_SIZE } from './const'
-import MainMenu from './views/game-menus'
+import { BallSize, GAME_SIZE, Guns, Keymap, PowerUp } from './const'
+import Game from './game/game'
+import { RectangularBody } from './game/physics/bodies'
+import { BallBody, LadderBody, PlatformBody } from './game/physics/objects'
+import PlayerBody from './game/physics/player'
+import PowerUpBody from './game/physics/power-ups'
+import { BulletBody, PowerWireBody } from './game/physics/weapons'
 import Loading from './views/loading'
+import Menu from './views/menu'
 
 /**
  * Tworzy aplikację we wskazanym kontenerze i określa dodatkowe parametry aplikacji.
@@ -32,9 +38,86 @@ _this.addChild(assetsLoader)
 
 /**
  * Funkcja która zostanie wykonana po załadowaniu zasobów.
+ * Wyświetla główne menu gry.
  */
 function onComplete() {
-    const menu = new MainMenu()
+    const menu = new Menu(
+        [
+            {
+                onClick: () => dummyGame(),
+                properties: {
+                    label: 'Play',
+                }
+            },
+            {
+                onClick: () => console.log('options'),
+                properties: {
+                    label: 'Options',
+                }
+            },
+            {
+                onClick: () => console.log('exit'),
+                properties: {
+                    label: 'Exit',
+                }
+            }
+        ],
+        {
+            width: 200,
+            height: 50,
+            texture: ImagesProvider.Instance().getTexture(ImagesProvider.image.MENU_BUTTON),
+            hoverTexture: ImagesProvider.Instance().getTexture(ImagesProvider.image.MENU_BUTTON_HOVER),
+            labelColor: 0x407ff9,
+            labelHoverColor: 0xff7ff9
+        },
+        false
+    )
+
     menu.position.set(app.view.width / 2, app.view.height / 2)
     _this.addChild(menu)
+}
+
+///////////////////////////////////////////////////////
+// Tymczasowa prosta gra.
+// TODO: Remove this.
+function dummyGame() {
+    const graphics = new Graphics()
+
+    _this.addChild(graphics)
+
+    _this.interactive = true
+
+    const game = new Game(
+        _this,
+        [
+            new PlayerBody({ x: 100, y: 430 }, { x: 50, y: 100 }, Keymap, Guns.POWER_WIRE)
+        ],
+        {
+            borders: [
+                new RectangularBody({ x: app.view.width / 2, y: 0 }, { x: app.view.width, y: 0 }, true),
+                new RectangularBody({ x: app.view.width / 2, y: app.view.height }, { x: app.view.width, y: 0 }, true),
+                new RectangularBody({ x: 0, y: app.view.height / 2 }, { x: 0, y: app.view.height }, true),
+                new RectangularBody({ x: app.view.width, y: app.view.height / 2 }, { x: 0, y: app.view.height }, true)
+            ],
+            platforms: [
+                new PlatformBody({ x: 200, y: 350 }, { x: 180, y: 20 })
+            ],
+            ladders: [
+                new LadderBody({ x: 200, y: 415 }, { x: 80, y: 150 })
+            ],
+            balls: [
+                new BallBody({ x: 300, y: 100 }, BallSize.SMALL)
+            ],
+            bullets: [
+                new BulletBody(250),
+                new PowerWireBody(550)
+            ],
+            powerUps: [
+                new PowerUpBody({ x: 400, y: 200 }, PowerUp.FORCE_FIELD)
+            ]
+        }
+    )
+    game.start(graphics)
+    
+    setTimeout(() => game.bullets.push(new BulletBody(500)), 5000)
 }
